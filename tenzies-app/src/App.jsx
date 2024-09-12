55e6cdc6eba69d6f0937518a2b90ff92034f8fdc
asilva-juarez <asilva-juarez@myseneca.ca>
2024-09-12 14:59:04 -0400

    Tenzies improved

--- tenzies-app/src/App.jsx
+++ tenzies-app/src/App.jsx
@@ -6,6 +6,12 @@ import { nanoid } from "nanoid";
 export default function App() {
   const [dice, setDice] = React.useState(allNewDice());
   const [tenzies, setTenzies] = React.useState(false);
+  const [numRolls, setNumRolls] = React.useState(0);
+  const [time, setTime] = React.useState(Number);
+  const [stopTime, setStopTime] = React.useState(false);
+  const [bestTime, setBestTime] = React.useState(
+    localStorage.getItem("time") || Infinity
+  );
 
   function allNewDice() {
     const newDice = [];
@@ -26,6 +32,8 @@ export default function App() {
 
   function rollDice() {
     if (!tenzies) {
+      setNumRolls((prevNum) => ++prevNum);
+      console.log(numRolls);
       setDice((oldDice) =>
         oldDice.map((die) => {
           return die.isHeld ? die : generateNewDie();
@@ -34,6 +42,9 @@ export default function App() {
     } else {
       setTenzies(false);
       setDice(allNewDice());
+      setNumRolls(0);
+      setTime(0);
+      setStopTime(false);
     }
   }
 
@@ -60,18 +71,39 @@ export default function App() {
     const allSameValue = dice.every((die) => die.value === firstValue);
     if (allHeld && allSameValue) {
       setTenzies(true);
-      console.log("You won!");
+      setStopTime(true);
+      if (time < +bestTime) {
+        localStorage.setItem("time", time);
+        setBestTime(time);
+      }
     }
-  }, [dice]);
+  }, [bestTime, dice, time]);
 
+  React.useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime((prevTime) => prevTime + 1); // increment by 1 second
+    }, 1000); // every 1 second
+
+    if (stopTime) clearInterval(intervalId);
+
+    return () => clearInterval(intervalId); // cleanup
+  }, [stopTime]); // run only once on mount
+
+  function fmtMSS(s) {
+    return (s - (s %= 60)) / 60 + (9 < s ? ":" : ":0") + s;
+  }
   return (
     <main>
       {tenzies && <Confetti />}
-      <h1 className="title">Tenzies</h1>
+      <h1 className="title">
+        <span className="time">TIme: {fmtMSS(time)}</span>
+        Tenzies <span className="counter">Num Rolls: {numRolls}</span>
+      </h1>
       <p className="instructions">
         Roll until all dice are the same. Click each die to freeze it at its
         current value between rolls.
       </p>
+      {bestTime !== Infinity && <p>Your best time is: {fmtMSS(bestTime)}</p>}
       <div className="dice-container">{diceElements}</div>
       <button onClick={rollDice} className="roll-dice">
         {tenzies ? "New Game" : "Roll"}
--- tenzies-app/src/components/Die.jsx
+++ tenzies-app/src/components/Die.jsx
@@ -1,11 +1,105 @@
-/* eslint-disable react/prop-types */
 export default function Die(props) {
   const styles = {
-    backgroundColor: props.isHeld ? "#59E391" : "white",
+    backgroundColor: props.isHeld ? "#59E391" : "tomato",
   };
-  return (
-    <div className="die-face" style={styles} onClick={props.holdDice}>
-      <h2 className="die-num">{props.value}</h2>
-    </div>
-  );
+
+  const face = () => {
+    switch (props.value) {
+      case 1:
+        return (
+          <div
+            className="first-face die"
+            style={styles}
+            onClick={props.holdDice}
+          >
+            <span className="dot"> </span>
+          </div>
+        );
+      case 2:
+        return (
+          <div
+            className="second-face die"
+            style={styles}
+            onClick={props.holdDice}
+          >
+            <span className="dot"> </span>
+            <span className="dot"> </span>
+          </div>
+        );
+      case 3:
+        return (
+          <div
+            className="third-face die"
+            style={styles}
+            onClick={props.holdDice}
+          >
+            <span className="dot"></span>
+            <span className="dot"></span>
+            <span className="dot"></span>
+          </div>
+        );
+      case 4:
+        return (
+          <div
+            className="fourth-face die"
+            style={styles}
+            onClick={props.holdDice}
+          >
+            <div className="column">
+              <span className="dot"></span>
+              <span className="dot"></span>
+            </div>
+            <div className="column">
+              <span className="dot"></span>
+              <span className="dot"></span>
+            </div>
+          </div>
+        );
+
+      case 5:
+        return (
+          <div
+            className="fifth-face die"
+            style={styles}
+            onClick={props.holdDice}
+          >
+            <div className="column">
+              <span className="dot"></span>
+              <span className="dot"></span>
+            </div>
+
+            <div className="column" style={styles} onClick={props.holdDice}>
+              <span className="dot"></span>
+            </div>
+
+            <div className="column" style={styles} onClick={props.holdDice}>
+              <span className="dot"></span>
+              <span className="dot"></span>
+            </div>
+          </div>
+        );
+
+      case 6:
+        return (
+          <div
+            className="fourth-face die"
+            style={styles}
+            onClick={props.holdDice}
+          >
+            <div className="column">
+              <span className="dot"></span>
+              <span className="dot"></span>
+              <span className="dot"></span>
+            </div>
+            <div className="column" style={styles} onClick={props.holdDice}>
+              <span className="dot"></span>
+              <span className="dot"></span>
+              <span className="dot"></span>
+            </div>
+          </div>
+        );
+    }
+  };
+
+  return face();
 }
